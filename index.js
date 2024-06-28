d91902478c9c36b105e5d9b13430a887872a6b3a
divakart208 <divakart208@gmail.com>
2024-06-28 13:35:43 +0000

    project2

--- index.js
+++ index.js
@@ -1,49 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const productsData = [
-    {
-      id: 1,
-      name: 'Smartphone',
-      price: '$499',
-      image:
-        'https://tse4.mm.bing.net/th?id=OIP.SM32I1WedoZ75g0uq24OCQHaFj&pid=Api&P=0&h=220',
-    },
-    {
-      id: 2,
-      name: 'Laptop',
-      price: '$1099',
-      image:
-        'https://tse2.mm.bing.net/th?id=OIP.puzai_D4808Fi7vlZTj3dQHaFh&pid=Api&P=0&h=220',
-    },
-    {
-      id: 3,
-      name: 'Headphones',
-      price: '$199',
-      image:
-        'https://tse2.mm.bing.net/th?id=OIP.E0HqFb3QUflcVW2tgRROUwHaHa&pid=Api&P=0&h=220',
-    },
-    {
-      id: 4,
-      name: 'Headphones',
-      price: '$999',
-      image:
-        'https://tse3.mm.bing.net/th?id=OIP.s_nc11N1vsQvlME1SS5NagHaHa&pid=Api&P=0&h=220',
-    },
-  ]
+const wrapper = document.querySelector(".wrapper"),
+    qrInput = wrapper.querySelector(".form input"),
+    generateBtn = wrapper.querySelector(".form button"),
+    qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
 
-  function generateProductCards(products) {
-    const productContainer = document.querySelector('.product-list')
-    products.forEach(product => {
-      const card = document.createElement('div')
-      card.classList.add('product-card')
-      card.innerHTML = `
-        <img src="${product.image}" alt="${product.name}">
-        <h3>${product.name}</h3>
-        <p>${product.price}</p>
-        <button>Add to Cart</button>
-      `
-      productContainer.appendChild(card)
-    })
-  }
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if (!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
 
-  generateProductCards(productsData)
-})
+qrInput.addEventListener("keyup", () => {
+    if (!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
