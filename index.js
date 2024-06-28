5061e2ed5b5ceaabff420cbc4739c882f328f912
divakart208 <divakart208@gmail.com>
2024-06-28 06:11:35 +0000

    myProject

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const productsData = [{
-            id: 1,
-            name: "Smartphone",
-            price: "$499",
-            image: "https://tse4.mm.bing.net/th?id=OIP.SM32I1WedoZ75g0uq24OCQHaFj&pid=Api&P=0&h=220"
-        },
-        {
-            id: 2,
-            name: "Laptop",
-            price: "$1099",
-            image: "https://tse2.mm.bing.net/th?id=OIP.puzai_D4808Fi7vlZTj3dQHaFh&pid=Api&P=0&h=220"
-        },
-        {
-            id: 3,
-            name: "Headphones",
-            price: "$199",
-            image: "https://tse2.mm.bing.net/th?id=OIP.E0HqFb3QUflcVW2tgRROUwHaHa&pid=Api&P=0&h=220"
-        },
-        {
-            id: 4,
-            name: "Headphones",
-            price: "$999",
-            image: "https://tse3.mm.bing.net/th?id=OIP.s_nc11N1vsQvlME1SS5NagHaHa&pid=Api&P=0&h=220"
-        },
-    ];
+document.addEventListener('DOMContentLoaded', function () {
+  const productsData = [
+    {
+      id: 1,
+      name: 'Smartphone',
+      price: '$499',
+      image:
+        'https://tse4.mm.bing.net/th?id=OIP.SM32I1WedoZ75g0uq24OCQHaFj&pid=Api&P=0&h=220',
+    },
+    {
+      id: 2,
+      name: 'Laptop',
+      price: '$1099',
+      image:
+        'https://tse2.mm.bing.net/th?id=OIP.puzai_D4808Fi7vlZTj3dQHaFh&pid=Api&P=0&h=220',
+    },
+    {
+      id: 3,
+      name: 'Headphones',
+      price: '$199',
+      image:
+        'https://tse2.mm.bing.net/th?id=OIP.E0HqFb3QUflcVW2tgRROUwHaHa&pid=Api&P=0&h=220',
+    },
+    {
+      id: 4,
+      name: 'Headphones',
+      price: '$999',
+      image:
+        'https://tse3.mm.bing.net/th?id=OIP.s_nc11N1vsQvlME1SS5NagHaHa&pid=Api&P=0&h=220',
+    },
+  ]
 
-    function generateProductCards(products) {
-        const productContainer = document.querySelector('.product-list');
-        products.forEach(product => {
-            const card = document.createElement('div');
-            card.classList.add('product-card');
-            card.innerHTML = `
+  function generateProductCards(products) {
+    const productContainer = document.querySelector('.product-list')
+    products.forEach(product => {
+      const card = document.createElement('div')
+      card.classList.add('product-card')
+      card.innerHTML = `
         <img src="${product.image}" alt="${product.name}">
         <h3>${product.name}</h3>
         <p>${product.price}</p>
         <button>Add to Cart</button>
-      `;
-            productContainer.appendChild(card);
-        });
-    }
+      `
+      productContainer.appendChild(card)
+    })
+  }
 
-    generateProductCards(productsData);
-});
+  generateProductCards(productsData)
+})
